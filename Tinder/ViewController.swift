7b34657e0ce7c1ee8c208ff20d21ab9866855b29
n0an <nantog@gmail.com>
2018-12-23 11:37:32 +0300

    init

---
+++ Tinder/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Tinder
+//
+//  Created by nag on 23/12/2018.
+//  Copyright © 2018 Anton Novoselov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let subviews = [UIColor.gray, UIColor.darkGray, UIColor.black].map { (color) -> UIView in
+            let v = UIView()
+            v.backgroundColor = color
+            return v
+        }
+        
+        let topStackView = UIStackView(arrangedSubviews: subviews)
+        topStackView.distribution = .fillEqually
+        
+        topStackView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        let blueView = UIView()
+        blueView.backgroundColor = .blue
+        
+        let yellowView = UIView()
+        yellowView.backgroundColor = .yellow
+        yellowView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        let stackView = UIStackView(arrangedSubviews: [topStackView, blueView, yellowView])
+
+        stackView.axis = .vertical
+        
+        view.addSubview(stackView)
+        
+        stackView.fillSuperview()
+        
+        
+    }
+
+
+}
+
