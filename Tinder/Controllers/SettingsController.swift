fa0bad8e12c07bc636d31d7bba8fa3c754bce8ab
n0an <nantog@gmail.com>
2019-03-26 23:40:57 +0300

    SettingsController implementing

--- Tinder/Controllers/SettingsController.swift
+++ Tinder/Controllers/SettingsController.swift
@@ -8,14 +8,103 @@
 
 import UIKit
 
+class CustomImagePickerController: UIImagePickerController {
+    var imageButton: UIButton?
+}
+
 class SettingsController: UITableViewController {
 
+    lazy var image1Button = createButton(selector: #selector(handleSelectPhoto))
+    lazy var image2Button = createButton(selector: #selector(handleSelectPhoto))
+    lazy var image3Button = createButton(selector: #selector(handleSelectPhoto))
+    
+    func createButton(selector: Selector) -> UIButton {
+        let button = UIButton(type: .system)
+        button.setTitle("Select Photo", for: .normal)
+        button.backgroundColor = .white
+        button.addTarget(self, action: selector, for: .touchUpInside)
+        button.imageView?.contentMode = .scaleAspectFill
+        button.layer.cornerRadius = 8
+        button.clipsToBounds = true
+        return button
+    }
+    
+    @objc fileprivate func handleSelectPhoto(button: UIButton) {
+        print(button)
+        let imagePickerController = CustomImagePickerController()
+        imagePickerController.imageButton = button
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupNavigationItems()
+        
+        tableView.backgroundColor = UIColor(white: 0.95, alpha: 1)
+        
+        tableView.tableFooterView = UIView()
+    }
+    
+    fileprivate func setupNavigationItems() {
         navigationItem.title = "Settings"
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancel))
+        
+        navigationItem.rightBarButtonItems = [
+            UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(handleSave)),
+            UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))]
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = UIView()
+        header.backgroundColor = .blue
+        
+        header.addSubview(image1Button)
+        let padding: CGFloat = 16
+        image1Button.anchor(top: header.topAnchor, leading: header.leadingAnchor, bottom: header.bottomAnchor, trailing: nil, padding: .init(top: padding, left: padding, bottom: padding, right: 0))
+        image1Button.widthAnchor.constraint(equalTo: header.widthAnchor, multiplier: 0.45).isActive = true
+        
+        let stackView = UIStackView(arrangedSubviews: [image2Button, image3Button])
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = padding
+        
+        header.addSubview(stackView)
+        stackView.anchor(top: header.topAnchor, leading: image1Button.trailingAnchor, bottom: header.bottomAnchor, trailing: header.trailingAnchor, padding: .init(top: padding, left: padding, bottom: padding, right: padding))
+        
+        return header
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 300
     }
 
+    @objc fileprivate func handleCancel() {
+        dismiss(animated: true)
+    }
+    
+    @objc fileprivate func handleSave() {
+        dismiss(animated: true)
+    }
+    
+    @objc fileprivate func handleLogout() {
+        dismiss(animated: true)
+    }
 
+}
+
+extension SettingsController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let selectedImage = info[.originalImage] as? UIImage
+        let customPicker = picker as? CustomImagePickerController
+        customPicker?.imageButton?.setImage(selectedImage?.withRenderingMode(.alwaysOriginal), for: .normal)
+        dismiss(animated: true)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true)
+    }
 }
