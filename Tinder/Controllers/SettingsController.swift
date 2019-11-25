5dcdb072a61d02031596680123c89f666cfb7ec9
n0an <nantog@gmail.com>
2019-11-25 23:11:50 +0300

    assets updated cleanup

--- Tinder/Controllers/SettingsController.swift
+++ Tinder/Controllers/SettingsController.swift
@@ -131,6 +131,7 @@ class SettingsController: UITableViewController {
         return header
     }()
     
+    // !!!IMPORTANT!!!
     class HeaderLabel: UILabel {
         override func drawText(in rect: CGRect) {
             super.drawText(in: rect.insetBy(dx: 16, dy: 0))
